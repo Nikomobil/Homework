99166bc83135d61634041dfae6bd40eb69f099a8
dennis <denya10.02@googlemail.com>
2025-04-23 17:07:47 +0200

    WebShop 23.04 Commit

--- src/test/java/com/webshop/tests/TestBase.java
+++ src/test/java/com/webshop/tests/TestBase.java
@@ -2,6 +2,8 @@ package com.webshop.tests;
 
 import com.webshop.fw.ApplicationManager;
 import org.openqa.selenium.remote.Browser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
@@ -9,19 +11,30 @@ import org.testng.annotations.BeforeSuite;
 
 public class TestBase {
 
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
+
     protected static ApplicationManager app = new ApplicationManager(System.getProperty("browser",
             Browser.CHROME.browserName()));
 
-    //@BeforeMethod
+
     @BeforeSuite
     public void setUp() {
         app.init();
     }
 
-    //@AfterMethod(enabled = false)
-    @AfterSuite(enabled = false)
+
+    @AfterSuite//(enabled = false)
     public void tearDown() {
         app.stop();
+    }
+
+    @BeforeMethod
+    public void startTest(){
+        logger.info("Start test");
+    }
+    @AfterMethod
+    public void stopTest(){
+        logger.info("Stop test");
     }
 
 
